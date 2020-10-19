800694d191888b38d944fc700442bc4857a152a1
kyle van der Wolf <kylevanderwolf@gmail.com>
2020-10-19 15:37:29 +0200

    Basic Arrow Functions

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// Deel A: 
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions()
+
+// Deel B:
+const fivePlusSeven = () => (5 + 7)
+console.log(fivePlusSeven());
+
+//Deel C:
+const myFunction = (a, b) => (a + b)
+console.log(myFunction(2, 5));
+
+//Deel D:
+const addFive = (a) => (a + 5)
+console.log(addFive(20));
+
+//Deel C:
+const createObject = () => ({ greeting: "hoi" })
+console.log(createObject());
+
